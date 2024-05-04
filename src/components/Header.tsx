da5b4a92f578723e4da9f26d663a618de490a2c2
alex-go-nguyen <alex.nguyen.goldenowl@gmail.com>
2024-05-04 13:01:52 +0700

    fix header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -77,7 +77,7 @@ export function Header() {
     <>
       <div
         className={cx(
-          'bg-[#f7efec] flex flex-col gap-8 pt-8 sticky w-screen z-10 top-0 transition-all ease-in-out duration-300 relative',
+          'bg-[#f7efec] flex flex-col gap-8 pt-8 sticky w-screen z-10 top-0 transition-all ease-in-out duration-300 relative pb-8 lg:pb-0',
           { 'translate-y-0': visible, '-translate-y-full': !visible },
         )}
       >
