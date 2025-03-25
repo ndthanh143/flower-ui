7774aa2e5e8db295e1fc386277852d3d10bfda13
ndthanh143 <nguyenduythanh421.tna@gmail.com>
2025-03-25 19:28:55 +0700

    Modify Button Place Order Zalo

--- src/app/p/[slug]/_containers/PlaceOrderButton.tsx
+++ src/app/p/[slug]/_containers/PlaceOrderButton.tsx
@@ -6,15 +6,26 @@ import { Product } from '@/services/product/types';
 
 export function PlaceOrderButton({ product }: { product: Product }) {
   const urlSite = `${configs.uriClient}/p/${product.slug}`;
-  const sendToZalo = () => {
+  const sendToFacebook = () => {
     const message = encodeURIComponent(`Xin Chào, mình muốn hỏi mua sản phẩm: ${product.name}, link: ${urlSite}`);
     const zaloLink = `https://m.me/108746482150895?text=${message}`;
     window.open(zaloLink, '_blank');
   };
 
+  const sendToZalo = () => {
+    const message = encodeURIComponent(`Xin Chào, mình muốn hỏi mua sản phẩm: ${product.name}, link: ${urlSite}`);
+    const zaloLink = `zalo://send?phone=84705740407&text=${message}`;
+    window.open(zaloLink, '_blank');
+  };
+
   return (
-    <Button className='!py-4 !text-base' onClick={sendToZalo}>
-      Đặt hàng qua Facebook
-    </Button>
+    <div className='flex flex-col gap-2'>
+      <Button className='!py-4 !text-base' onClick={sendToFacebook}>
+        Đặt hàng qua Facebook
+      </Button>
+      <Button className='!py-4 !text-base' onClick={sendToZalo}>
+        Đặt hàng qua Zalo
+      </Button>
+    </div>
   );
 }
